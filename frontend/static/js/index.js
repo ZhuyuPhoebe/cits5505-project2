78d01d19fb35eaefcd611d86dd686d7df5dd5413
lyxxh718 <lyxxh718@gmail.com>
2021-05-09 00:06:50 +0800

    Refine code

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,34 +1,49 @@
 (function () {
-    var username = sessionStorage.getItem("username");
+    var username = localStorage.getItem("username");
     if (username) {
+        // get units
         $.get('http://localhost:5000/api/units',
-        {},
-        function(data) {
-            if(data.code === 200) {
-                var units = data.data;
-                var html = '';
-                for(var i = 0; i < units.length; i++) {
-                    var unit = units[i];
-                    var id = unit[0];
-                    var title = unit[1];
-                    html += "<tr id='" + id + "'><td>" + id + "</td><td>" + title + "</td><td><a class='btn' style='margin:12px;' data-id='"+ id +"' onclick='learn(event)' >Learn</a><a class='btn' style='margin:12px 0;' data-id='"+ id +"' onclick='quiz(event)' >Quiz</a></td></tr>";
+            {},
+            function (data) {
+                if (data.code === 200) {
+                    var units = data.data;
+                    var html = '';
+                    for (var i = 0; i < units.length; i++) {
+                        var unit = units[i];
+                        var id = unit[0];
+                        var title = unit[1];
+                        html += "<tr id='" + id + "'>";
+                        html += "<td>" + id + "</td>";
+                        html += "<td>" + title + "</td>";
+                        html += "<td>";
+                        // Learn button
+                        html += "<a class='btn' style='margin: 12px;' data-id='" + id + "' onclick='learn(event)' >Learn</a>";
+                        // Quiz button
+                        html += "<a class='btn' style='margin:12px 0;' data-id='" + id + "' onclick='quiz(event)' >Quiz</a>";
+                        html += "</td>";
+                        html += "</tr>";
+                    }
+                    // append to DOM
+                    $('#units').append(html);
                 }
-                $('#units').append(html);
-            }
-        });
+            });
     }
 })();
 
-function learn (e) {
+function learn(e) {
+    // Redirect to learning page
     e.preventDefault();
     var unitId = $(e.target).data('id');
-    sessionStorage.setItem("unit", unitId);
+    // Storage unit id
+    localStorage.setItem("unit", unitId);
     location.href = './learn.html';
 }
 
-function quiz (e) {
+function quiz(e) {
+    // Redirect to quiz page
     e.preventDefault();
     var unitId = $(e.target).data('id');
-    sessionStorage.setItem("quiz", unitId);
+    // Storage quiz id
+    localStorage.setItem("quiz", unitId);
     location.href = './quiz.html';
 }
